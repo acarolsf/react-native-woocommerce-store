eec9839ad541a027b75b61c631eb0d0d78ad9ce1
Ana Carolina <cscarol20@gmail.com>
2019-09-16 12:03:00 -0300

    App part 1

---
+++ src/constants/Constants.js
@@ -0,0 +1,23 @@
+// const Constants = {
+//     URL: {
+//  wc: 'https://woocommerce-store.on-its-way.com/wp-json/wc/v2/'
+//     },
+//     Keys: {
+//  ConsumerKey: 'ck_b429b4682610f81fca1cc4a408eddd92d1788d6b',
+//  ConsumerSecret: 'cs_0ae6d8ed8e0f2d57d436e52703fb6a3de92c6512'
+//     }
+//  }
+//  export default Constants;
+ 
+
+ const Constants = {
+   URL: {
+       wc: 'https://woocommerce-store.on-its-way.com/wp-json/wc/v2/'
+   },
+   Keys: {
+       ConsumerKey: 'ck_286be1c0fd553c4b803ba2b12c72593eb4676f8d',
+      ConsumerSecret: 'cs_d4cc172fabaeef203a78f119518df0453753db65'
+   }
+}
+
+export default Constants;
